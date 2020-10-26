d79bf177e9e09cb51ee29b5c2254e410def40b52
denis <denis@mail.com>
2020-10-26 21:42:11 +0300

    Start: Brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let strReuslt = str;
+    
+  let configArr = bracketsConfig.map(element => {
+    return element.join('');
+  })
+
+
+  while (strReuslt.includes('()') || strReuslt.includes('[]') || strReuslt.includes('{}') || strReuslt.includes('||') || strReuslt.includes('12') || strReuslt.includes('34') || strReuslt.includes('56') || strReuslt.includes('77') || strReuslt.includes('88')) {
+    configArr.forEach(element => {
+      strReuslt = strReuslt.replace(element, '')
+  })
+  } 
+
+  return strReuslt == '';
 }
+
+
